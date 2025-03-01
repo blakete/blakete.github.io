07cb886fdab37b42f6033bc5f7cf8fe8085eb32c
Blake Edwards <blakeedwards823@gmail.com>
2025-03-01 18:49:22 -0500

    new post for needle in a haystack

---
+++ _posts/create_post.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Ensure exactly one argument is provided.
+if [ "$#" -ne 1 ]; then
+  echo "Usage: $0 \"Title of the post\""
+  exit 1
+fi
+
+# Capture the title from the command line argument.
+title="$1"
+
+# Get the current date in YYYY-MM-DD format.
+current_date=$(date +"%Y-%m-%d")
+
+# Generate the filename by replacing spaces in the title with dashes.
+filename="${current_date}-$(echo "$title" | tr ' ' '-')".md
+
+# Create (or overwrite) the file with the blog post header.
+cat > "$filename" <<EOF
+---
+layout: post
+title: "$title"
+author:
+- Blake Edwards
+---
+EOF
+
+echo "Markdown file created: $filename"
+
+# Open the created file in nvim.
+nvim "$filename"
