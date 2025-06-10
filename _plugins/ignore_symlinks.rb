7e8f4a3ce5fc25e681886e7d187df90b78a7365d
Blake Edwards <blakeedwards823@gmail.com>
2025-06-10 14:52:32 -0400

    fix for symlink files appearing on view pages

--- _plugins/ignore_symlinks.rb
+++ _plugins/ignore_symlinks.rb
@@ -1,62 +1,18 @@
-# _plugins/ignore_symlinks.rb
-
-Jekyll::Hooks.register :site, :after_reset do |site|
-  # Filter out symlinks from collections
-  site.collections.each do |name, collection|
-    collection.docs.reject! do |doc|
-      File.symlink?(doc.path)
-    end
-  end
-  
-  # Filter out symlinks from pages
-  site.pages.reject! do |page|
-    File.symlink?(page.path)
-  end
-  
-  # Filter out symlinks from static files
-  site.static_files.reject! do |file|
-    File.symlink?(file.path)
-  end
-end
-
-# Additional hook to ensure symlinks are filtered after site reads files
-Jekyll::Hooks.register :site, :post_read do |site|
-  # Filter collections again in case new docs were added
-  site.collections.each do |name, collection|
-    collection.docs.reject! do |doc|
-      File.symlink?(doc.path)
-    end
-  end
-  
-  # Filter pages again
-  site.pages.reject! do |page|
-    File.symlink?(page.path)
-  end
-  
-  # Filter static files again
-  site.static_files.reject! do |file|
-    File.symlink?(file.path)
-  end
-end
-
-# Custom generator to ensure symlinks are excluded during site generation
-class IgnoreSymlinksGenerator < Jekyll::Generator
-  priority :highest
-  
-  def generate(site)
-    # Final pass to remove any symlinks that might have been missed
-    site.collections.each do |name, collection|
-      collection.docs.reject! do |doc|
-        File.symlink?(doc.path)
+Jekyll::Hooks.register :site, :pre_render do |site|
+  if site.collections['private_local']
+    site.collections['private_local'].docs.each do |doc|
+      # Mark symbolic links and capture their view directory
+      if File.symlink?(doc.path)
+        doc.data['is_symlink'] = true
+        
+        # Extract view directory from symlink path
+        if doc.path.include?('views/')
+          view_dir = doc.path.split('views/')[1].split('/')[0]
+          doc.data['view_category'] = view_dir
+        end
+      else
+        doc.data['is_symlink'] = false
       end
-    end
-    
-    site.pages.reject! do |page|
-      File.symlink?(page.path)
-    end
-    
-    site.static_files.reject! do |file|
-      File.symlink?(file.path)
     end
   end
 end
