99b75121ab27d42386ac94c6896f440c91ec96e0
Blake Edwards <blakeedwards823@gmail.com>
2025-06-27 05:51:38 -0400

    fixed auto-reloading

--- _plugins/ignore_symlinks.rb
+++ _plugins/ignore_symlinks.rb
@@ -1,8 +1,12 @@
-Jekyll::Hooks.register :site, :pre_render do |site|
+Jekyll::Hooks.register :site, :post_read do |site|
   if site.collections['private_local']
-    site.collections['private_local'].docs.each do |doc|
-      # Mark symbolic links and capture their view directory
+    # Filter out symbolic links from the docs collection to prevent conflicts
+    original_docs = site.collections['private_local'].docs
+    non_symlink_docs = []
+    
+    original_docs.each do |doc|
       if File.symlink?(doc.path)
+        # Mark symbolic links but don't include them in processing
         doc.data['is_symlink'] = true
         
         # Extract view directory from symlink path
@@ -10,9 +14,16 @@ Jekyll::Hooks.register :site, :pre_render do |site|
           view_dir = doc.path.split('views/')[1].split('/')[0]
           doc.data['view_category'] = view_dir
         end
+        
+        # Skip adding symlinks to the docs collection for processing
+        next
       else
         doc.data['is_symlink'] = false
+        non_symlink_docs << doc
       end
     end
+    
+    # Replace the docs collection with only non-symlink documents
+    site.collections['private_local'].docs = non_symlink_docs
   end
 end
